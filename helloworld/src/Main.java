50eeabbe6fd7f74ef621baeb00f73c58d58d1b3b
yadhikari <yakeenstan@gmail.com>
2021-05-24 22:47:07 -0500

    Add while loop and exit condition

--- helloworld/src/Main.java
+++ helloworld/src/Main.java
@@ -1,13 +1,22 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a greeting: ");
+        String greeting = scanner.next();
+        String name = "";
+        do {
+            System.out.print("Enter a name: ");
+            name = scanner.next();
 
-        System.out.println("What is your name? ");
-        String name = scanner.nextLine();
-
-        System.out.println("Hello " + name);
+            System.out.println(greeting + " " + name);
+        } while (!name.equals("exit"));
+        System.out.println("You have exited the module!");
     }
+
 }
